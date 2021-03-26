953c5bfe509b8b3e9c0f6b1b1c134f51522b66c9
Javier Leguízamo <javier.leguizamo.ie.tesvg@gmail.com>
2021-03-26 10:41:03 -0600

    Primer programa

--- Primer_Programa/Primer_Programa/main.c
+++ Primer_Programa/Primer_Programa/main.c
@@ -1,16 +1,31 @@
-/*
- * Primer_Programa.c
- *
- * Created: 26/03/2021 09:04:39 a. m.
- * Author : jimmy
- */ 
-
+#define F_CPU 8e6
 #include <avr/io.h>
+#include <avr/interrupt.h>
+int y[4],xn;
 
+ISR(ADC_vect){
+	xn=ADC;
+	y[0]=5*xn+2*y[1]+4*y[2]+3*y[3];
+	y[1]=y[0];
+	y[2]=y[1];
+	y[3]=y[2];
+}
+
+ISR(TIMER0_COMPA_vect){
+	
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+    DDRB |= 0xff;
+	ADMUX |= 1<<REFS0;
+	ADCSRA |= _BV(ADEN) | _BV(ADATE) | _BV(ADIE) | _BV(ADPS0);
+	ADCSRB |= _BV(ADTS0) | _BV(ADTS1);
+	OCR0A = 255;
+	TCCR0A |=_BV(WGM01);
+	TCCR0B |= _BV(CS01);
+	TIMSK0 |= _BV(OCIE0A);
+	sei();
     while (1) 
     {
     }
