87ea971a4c76794cfee1c2d90da0fdff6ba7ccbc
Javier Leguízamo <javier.leguizamo.ie.tesvg@gmail.com>
2021-03-26 09:05:45 -0600

    Primer commit

---
+++ Primer_Programa/Primer_Programa/main.c
@@ -0,0 +1,18 @@
+/*
+ * Primer_Programa.c
+ *
+ * Created: 26/03/2021 09:04:39 a. m.
+ * Author : jimmy
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
